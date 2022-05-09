6f90155d72d6b8f76ce11d3a16a59df54ee90395
Duc Tran <tranhuynhduc.kt@gmail.com>
2022-05-09 12:15:47 +0700

    Add basic Blixo sdk

---
+++ index.js
@@ -0,0 +1,21 @@
+import Blixo from "./lib/blixo";
+
+console.log('Blixo SDK');
+
+const blixo = Blixo();
+
+// blixo.items.list().then(res => {
+//   const { response, errors } = res;
+
+//   if (errors) {
+//     return;
+//   }
+
+//   console.log('LIST ITEMS success', response);
+// })
+
+
+// blixo.items.add().then(res => {
+//   console.log('ADD ITEMS success', res);
+// })
+
